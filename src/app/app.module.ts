4bcba10dd387699fd48b79f9c41db12f9b2e59a3
andrey-abramow <abramow.andrey@gmail.com>
2019-05-19 14:28:07 +0300

    layout: Finish header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './sidebar/nav-menu/nav-menu.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderProfileComponent } from './header-profile/header-profile.component';
+import { HeaderNotificationsComponent } from './header-notifications/header-notifications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    SidebarComponent
+    SidebarComponent,
+    AutocompleteComponent,
+    BreadcrumbsComponent,
+    HeaderProfileComponent,
+    HeaderNotificationsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header-notifications/header-notifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-notifications',
+  templateUrl: './header-notifications.component.html',
+  styleUrls: ['./header-notifications.component.scss']
+})
+export class HeaderNotificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
